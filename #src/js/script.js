af813e2ee2b7e5e87af78e3e2503e07330034f95
MaximTrz <maxim.trz@gmail.com>
2021-09-12 21:14:03 +0500

    добавил валидацию имени и телефона

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,6 +5,8 @@ formQuestion.addEventListener('submit', async (e)=>{
 
     validateForm(formQuestion);
 
+    return;
+
     let response = await fetch('https://60376bfd5435040017722533.mockapi.io/form', {
         method: 'POST',
         body: new FormData(formQuestion)
@@ -17,9 +19,29 @@ formQuestion.addEventListener('submit', async (e)=>{
       } else {
           alert('Что-то пошло не так');
       }    
-})
+});
 
 
 const validateForm = (form)=>{
-    console.log(form);
+    const inputs = form.querySelectorAll('.validate'),
+          fio = /^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}\-([А-ЯA-Z][\x27а-яa-z]{1,}|(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$/,
+          phone = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
+    const test = (value, reg)=>{
+        const regExp = reg;
+        return regExp.test(value)
+    }
+
+    inputs.forEach(element => {
+        element.addEventListener('focus', ()=>element.classList.remove('wrong'));
+        
+        const val = element.value;
+        if (!val){
+            element.classList.add('wrong');
+        } else {
+            
+            console.log(test(val, phone));
+
+        } 
+    });
 }
